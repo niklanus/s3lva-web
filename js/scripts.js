c037bc2f7603492e4caf3a0fc368151b7acb43f6
niklanus <nik@credia.cm>
2015-03-02 02:18:37 -0300

    Avances.

---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+$(window).on("load resize",function(e){
+	viewportHeight = $(window).height();
+	$('.intro-1').height(viewportHeight);
+	$('.intro-2').height(viewportHeight);
+
+	$('.intro-2-msg').css('top', viewportHeight);
+	$('.mundo').css('top', viewportHeight);
+
+	/*
+	if($(window).width() <= 680){
+		$('.nav-side-items').height(viewportHeight - 230)
+	} else {
+		$('.nav-side-items').height(viewportHeight - 76)
+	}*/
+});
+
+$( document ).ready(function() {
+	/*$("#introText").Morphext({
+		animation: "",
+		separator: ",",
+		speed: 300
+	});*/
+
+	$('.smooth').smoothScroll({
+		speed: 1000,
+		easing: 'easeInOutQuart'
+	});
+
+	$('.work-slider').slick({
+		autoplay: true,
+		autoplaySpeed: 1000,
+		arrows: false,
+		fade: true
+	});
+});
+
+$(".nav-btn").click(function() {
+	if ( $('body').hasClass('nav-visible') ) {
+		$('body').removeClass('nav-visible');
+		$(".nav-main li")
+		  .velocity("transition.fadeOut", {
+		    duration: 150,
+		    stagger: 0,
+		    delay: 0
+		});
+	} else {
+		$('body').addClass('nav-visible');
+	 	$(".nav-main li")
+		  .velocity("transition.slideUpIn", {
+		    duration: 600,
+		    stagger: 100,
+		    delay: 0
+		});
+	}
+});
+
+$(".nav-main a").click(function() {
+	$('body').removeClass('nav-visible');
+	$(".nav-main li")
+	  .velocity("transition.fadeOut", {
+	    duration: 150,
+	    stagger: 0,
+	    delay: 0
+	});
+});
+
+vh = $(window).height();
+
+var date = new Date();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var month = date.getMonth();
+var day = date.getDate();
+var year = date.getFullYear();
+var monthNames = [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ]; 
+document.getElementById("date").innerHTML = day+" de "+monthNames[month]+" de "+year;
+document.getElementById("year").innerHTML = year;
+
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({triggerElement: "#intro-1", triggerHook: "0", offset: 0, duration: vh})
+	.setTween(TweenMax.to('#intro-1', 0.5, {
+    opacity: 0.5,
+    scale: 0.85
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#intro-2", triggerHook: "0", offset: 0, duration: vh*2})
+	.setPin(".intro-2-bg")
+	.addIndicators()
+	.addTo(controller);
+
+var waypoint1 = $('.intro-2-msg').waypoint(function(direction) {
+	$('.intro-2-msg p').blast({ delimiter: "word" }).velocity("transition.slideDownIn", {
+	    duration: 1600,
+	    stagger: 50,
+	    delay: 0
+	});
+	waypoint1.destroy();
+}, {
+  offset: '90%'
+})
