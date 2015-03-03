426a020f39d77540309e21a2a0732e03e07542af
niklanus <nik@credia.cm>
2015-03-02 23:14:37 -0300

    Fixed video BG.

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ $( document ).ready(function() {
 	});*/
 
 	$('.smooth').smoothScroll({
-		speed: 1000,
+		speed: 1500,
 		easing: 'easeInOutQuart'
 	});
 
@@ -82,16 +82,16 @@ var controller = new ScrollMagic.Controller();
 
 var scene = new ScrollMagic.Scene({triggerElement: "#intro-1", triggerHook: "0", offset: 0, duration: vh})
 	.setTween(TweenMax.to('#intro-1', 0.5, {
-    opacity: 0.5,
-    scale: 0.85
+    opacity: 0.2,
+    scale: 1
 	}))
 	.addIndicators()
 	.addTo(controller);
-
+/*
 var scene = new ScrollMagic.Scene({triggerElement: "#intro-2", triggerHook: "0", offset: 0, duration: vh*2})
-	.setPin(".intro-2-bg")
+	.setPin(".intro-2-bgvideo")
 	.addIndicators()
-	.addTo(controller);
+	.addTo(controller);*/
 
 var scene = new ScrollMagic.Scene({triggerElement: "#mundo", triggerHook: "0.8", offset: 0, duration: 193})
 	.setTween(TweenMax.from('#mundo-parallax-1', 0.5, {
@@ -109,6 +109,80 @@ var scene = new ScrollMagic.Scene({triggerElement: "#mundo", triggerHook: "0.8",
 	.addIndicators()
 	.addTo(controller);
 
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: -100, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-3', 0.5, {
+		opacity: 0,
+    	left: -100
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: -100, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-3 .number', 0.5, {
+		scale: 4
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: 100, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-4', 0.5, {
+		opacity: 0,
+    	right: -100
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: 100, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-4 .number', 0.5, {
+		scale: 4
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: 300, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-5', 0.5, {
+		opacity: 0,
+    	left: -100
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: 300, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-5 .number', 0.5, {
+		scale: 4
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: 500, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-6', 0.5, {
+		opacity: 0,
+    	right: -100
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: 500, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-6 .number', 0.5, {
+		scale: 4
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: 700, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-7', 0.5, {
+		opacity: 0,
+    	left: -100
+	}))
+	.addIndicators()
+	.addTo(controller);
+
+var scene = new ScrollMagic.Scene({triggerElement: "#mundo-parallax-2", triggerHook: "0.5", offset: 700, duration: 400})
+	.setTween(TweenMax.from('#mundo-parallax-7 .number', 0.5, {
+		scale: 4
+	}))
+	.addIndicators()
+	.addTo(controller);
 
 var waypoint1 = $('.intro-2-msg').waypoint(function(direction) {
 	$('.intro-2-msg p').blast({ delimiter: "word" }).velocity("transition.slideDownIn", {
